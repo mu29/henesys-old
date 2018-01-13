9b1711d1b736bf545f68e446ffeb1da4f2dbdbea
InJung Chung <mu29@yeoubi.net>
2018-01-13 21:47:09 +0900

    SweetAlert 패키지를 추가한다

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -3,20 +3,28 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import 'moment/locale/ko';
 
 export default class extends Document {
-  static async getInitialProps ({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage();
-    return { html, head, errorHtml, chunks };
+  static async getInitialProps({ renderPage }) {
+    const {
+      html,
+      head,
+      errorHtml,
+      chunks,
+    } = renderPage();
+    return {
+      html, head, errorHtml, chunks,
+    };
   }
 
-  render () {
+  render() {
     return (
-      <html>
+      <html lang="ko">
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/earlyaccess/nanumgothic.css" />
+          <link rel="stylesheet" href="/static/sweetalert2.min.css" />
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" />
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js" />
         </Head>
