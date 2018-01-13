33038ba83ef9c844220cc321a5eb56793f909be3
InJung Chung <mu29@yeoubi.net>
2018-01-13 10:37:39 +0900

    Alert 모듈을 추가한다

---
+++ src/app/modules/Alert.js
@@ -0,0 +1,49 @@
+export const SHOW_MODAL = 'SHOW_MODAL';
+export const HIDE_MODAL = 'HIDE_MODAL';
+
+const initialState = {
+  modal: {
+    show: false,
+    element: null,
+    props: {}
+  },
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SHOW_MODAL:
+      return {
+        ...state,
+        modal: {
+          show: true,
+          element: action.element,
+          props: action.props,
+        },
+      }
+    case HIDE_MODAL:
+      return {
+        ...state,
+        modal: {
+          show: false,
+          element: null,
+          props: {},
+        },
+      }
+    default:
+      return state;
+  }
+}
+
+export function showModal(element, props) {
+  return {
+    type: SHOW_MODAL,
+    element,
+    props,
+  };
+}
+
+export function hideModal() {
+  return {
+    type: HIDE_MODAL,
+  };
+}
--- src/app/modules/index.js
+++ src/app/modules/index.js
@@ -1,2 +1,2 @@
-export { default as Menu } from './Menu';
+export { default as Alert } from './Alert';
 export { default as Post } from './Post';
