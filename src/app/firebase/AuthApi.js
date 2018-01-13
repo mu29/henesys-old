5fe42be933944c57f547444a478d86de259dc56d
InJung Chung <mu29@yeoubi.net>
2018-01-13 21:47:33 +0900

    로그인 액션이 정상적으로 작동하도록 수정한다

--- src/app/firebase/AuthApi.js
+++ src/app/firebase/AuthApi.js
@@ -1,33 +1,36 @@
-import { loadAuth } from './Firebase';
+import { loadDB, loadAuth } from './Firebase';
 
 export async function register(email, password, name) {
+  const db = await loadDB();
   const auth = await loadAuth();
 
-  return auth.createUserWithEmailAndPassword(email, password)
-    .then(() => {
-      auth.currentUser.updateProfile({ displayName: name });
-      return {
-        user: {
-          email,
-          name,
-        },
-      };
-    })
-    .catch(error => error);
+  try {
+    const user = await auth.createUserWithEmailAndPassword(email, password);
+    await user.updateProfile({ displayName: name });
+    await db.collection('users').doc(user.uid).set({
+      email: user.email,
+      name: user.displayName,
+    });
+    return {
+      user: {
+        email,
+        name,
+      },
+    };
+  } catch (error) {
+    return { error };
+  }
 }
 
 export async function login(email, password) {
+  const db = await loadDB();
   const auth = await loadAuth();
 
-  return auth.signInWithEmailAndPassword(email, password)
-    .then(() => {
-      const user = auth.currentUser;
-      return {
-        user: {
-          email: user.email,
-          name: user.displayName,
-        },
-      };
-    })
-    .catch(error => error);
+  try {
+    const userInfo = await auth.signInWithEmailAndPassword(email, password);
+    const user = await db.collection('users').doc(userInfo.uid).get();
+    return { user: user.data() };
+  } catch (error) {
+    return { error };
+  }
 }
--- src/app/sagas/AuthSaga.js
+++ src/app/sagas/AuthSaga.js
@@ -1,41 +1,66 @@
 // @flow
 
-import { takeLatest, put, call } from 'redux-saga/effects';
+import { takeLatest, put, call, all } from 'redux-saga/effects';
 import {
   registerActionTypes,
   registerActions,
   loginActionTypes,
   loginActions,
 } from 'modules/Auth';
+import { showMessage, hideMessage, hideModal } from 'modules/Alert';
 import { register, login } from 'firebase/AuthApi';
 
 function* executeRegister({ email, password, name }) {
   const { user, error } = yield call(register, email, password, name);
   if (user) {
-    yield put(registerActions.success({ user }));
+    yield all([
+      put(registerActions.success({ user })),
+      put(hideMessage()),
+      put(hideModal()),
+    ]);
     return;
   }
 
-  let message = '';
+  let errorMessage = '';
   switch (error.code) {
     case 'auth/weak-password':
-      message = '비밀번호는 최소 6자리 이상이어야 합니다.';
+      errorMessage = '비밀번호는 최소 6자리 이상이어야 합니다.';
       break;
     case 'auth/email-already-in-use':
-      message = '이미 사용중인 이메일 주소입니다.';
+      errorMessage = '이미 사용중인 이메일 주소입니다.';
       break;
     case 'auth/invalid-email':
-      message = '이미 사용중인 이메일 주소입니다.';
+      errorMessage = '이메일 주소 형식이 아닙니다.';
       break;
     default:
-      message = error.code;
+      errorMessage = error.message;
   }
-  yield put(registerActions.failure({ error: message }));
+  yield put(showMessage(errorMessage));
 }
 
 function* executeLogin({ email, password }) {
   const { user, error } = yield call(login, email, password);
-  yield put(user ? loginActions.success({ user }) : loginActions.failure({ error }));
+  if (user) {
+    yield all([
+      put(loginActions.success({ user })),
+      put(hideMessage()),
+      put(hideModal()),
+    ]);
+    return;
+  }
+
+  let errorMessage = '';
+  switch (error.code) {
+    case 'auth/user-not-found':
+      errorMessage = '메일 주소를 확인해주세요.';
+      break;
+    case 'auth/wrong-password':
+      errorMessage = '비밀번호를 확인해주세요.';
+      break;
+    default:
+      errorMessage = error.message;
+  }
+  yield put(showMessage(errorMessage));
 }
 
 export function* watchRegister() {
