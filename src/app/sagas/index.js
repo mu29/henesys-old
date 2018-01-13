e05ff14fc721c144b6a93540cf0b685c62ab02fa
InJung Chung <mu29@yeoubi.net>
2018-01-13 10:37:27 +0900

    Menu 모듈을 제거한다

--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -1,10 +1,8 @@
 import { all, fork } from 'redux-saga/effects';
-import * as menuSaga from './MenuSaga';
 import * as postSaga from './PostSaga';
 
 export default function* root() {
   yield all([
-    ...Object.keys(menuSaga).map(key => fork(menuSaga[key])),
     ...Object.keys(postSaga).map(key => fork(postSaga[key])),
   ]);
 }
