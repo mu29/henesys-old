b9fd06cc5c9688c737a968cdc0410771ff954633
InJung Chung <mu29@yeoubi.net>
2018-01-15 00:37:36 +0900

    작성중인 글 메뉴를 표시한다

--- src/app/components/Editor/Editor.js
+++ src/app/components/Editor/Editor.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Menus from 'constants/Menu';
-import { Input } from 'components/Bootstrap';
+import { FormGroup, Input } from 'components/Bootstrap';
 import EditorTextArea from './EditorTextArea';
 import Toolbar from './Toolbar';
 
@@ -30,37 +30,17 @@ export default class Editor extends Component {
 
     return (
       <div className="editor">
-        <div className="title-area">
-          <h4>{ menu.name }</h4>
+        <FormGroup addon={ menu.name }>
           <Input wrapperClassName="title-input" placeholder="제목을 입력해주세요" />
-        </div>
+        </FormGroup>
         <Toolbar />
         <EditorTextArea onChange={ this.onChangeContent } html={ this.state.content } />
         <style jsx>{`
-          .editor .title-area {
-            display: flex;
-            flex-direction: row;
-            margin-bottom: 1rem;
-          }
-          .editor .title-area h4 {
-            display: flex;
-            align-items: center;
+          .editor input {
             height: 3rem;
-            margin: 0;
             padding: 1rem;
-            border: 1px solid #E7E7E7;
-            border-right: none;
-            border-top-left-radius: 0.25rem;
-            border-bottom-left-radius: 0.25rem;
-            background-color: #FAFAFA;
-          }
-          .editor .title-area .title-input {
-            flex: 1;
-          }
-          .editor .title-area input {
-            height: 3rem;
-            border-top-right-radius: 0.25rem;
-            border-bottom-right-radius: 0.25rem;
+            border-top-right-radius: 0.25rem !important;
+            border-bottom-right-radius: 0.25rem !important;
           }
         `}</style>
       </div>
