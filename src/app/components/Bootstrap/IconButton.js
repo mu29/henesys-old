aa58100c442be321faf14d612de38e11d07f136f
InJung Chung <mu29@yeoubi.net>
2018-01-15 21:15:48 +0900

    IconButton 컴포넌트에 자식이 있는 경우 아이콘과 함께 표시한다

--- src/app/components/Bootstrap/IconButton.js
+++ src/app/components/Bootstrap/IconButton.js
@@ -4,20 +4,54 @@ import Button from './Button';
 
 export default class IconButton extends Component {
   static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.element,
     icon: PropTypes.string.isRequired,
     size: PropTypes.number,
   };
 
   static defaultProps = {
+    className: '',
     size: 14,
+    children: null,
   };
 
   render() {
-    const { icon, size, ...props } = this.props;
+    const {
+      className,
+      children,
+      icon,
+      size,
+      ...props
+    } = this.props;
 
     return (
-      <Button { ...props }>
+      <Button
+        className={ `${className} ${children ? 'icon-button' : ''}` }
+        style={{ fontSize: size }}
+        { ...props }
+      >
         <i style={{ fontSize: size }} className={ `fa fa-${icon}` } />
+        { children }
+        <style jsx>{`
+          .icon-button {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            padding: 0.5rem 1rem;
+            border: none;
+            border-left: 0.3rem solid #F9D14C;
+            border-radius: 0.125rem;
+            text-align: center;
+          }
+          .icon-button:hover {
+            color: white;
+          }
+          .icon-button .fa {
+            margin-right: 0.5rem;
+          }
+        `}</style>
       </Button>
     );
   }
