025c29110eb187c6fcba06fc2f2ece7e46c51773
InJung Chung <mu29@yeoubi.net>
2018-01-21 02:01:20 +0900

    글 관련 액션이 로딩 액션을 전파하도록 수정한다

--- src/app/sagas/PostSaga.js
+++ src/app/sagas/PostSaga.js
@@ -1,6 +1,7 @@
 // @flow
 
-import { takeEvery, takeLatest, put, call } from 'redux-saga/effects';
+import { takeEvery, takeLatest, put, call, all } from 'redux-saga/effects';
+import { showLoading, hideLoading } from 'modules/Alert';
 import {
   createPostActionTypes,
   createPostActions,
@@ -13,29 +14,46 @@ import { createPost, readPostList, readPost } from 'firebase/PostApi';
 import { Router } from 'routes';
 
 function* addPost({ tag, title, content }) {
+  yield put(showLoading(createPostActionTypes.REQUEST));
   const { post, error } = yield call(createPost, tag, title, content);
 
   if (!post) {
-    yield put(createPostActions.failure({ error }));
+    yield all([
+      put(hideLoading()),
+      put(createPostActions.failure({ error })),
+    ]);
     return;
   }
 
   Router.pushRoute(`/posts?tag=${tag}`);
-  yield put(createPostActions.success({ post }));
+  yield all([
+    put(hideLoading()),
+    put(createPostActions.success({ post })),
+  ]);
 }
 
 function* loadPostList({ tag, last }) {
+  yield put(showLoading(fetchPostListActionTypes.REQUEST));
   const { posts, error } = yield call(readPostList, tag, last);
-  yield put(posts ?
-    fetchPostListActions.success({ posts }) :
-    fetchPostListActions.failure({ error }));
+
+  yield all([
+    put(hideLoading()),
+    put(posts ?
+      fetchPostListActions.success({ posts }) :
+      fetchPostListActions.failure({ error })),
+  ]);
 }
 
 function* loadPost({ id }) {
+  yield put(showLoading(fetchPostActionTypes.REQUEST));
   const { post, error } = yield call(readPost, id);
-  yield put(post ?
-    fetchPostActions.success({ post }) :
-    fetchPostActions.failure({ error }));
+
+  yield all([
+    put(hideLoading()),
+    post ?
+      fetchPostActions.success({ post }) :
+      fetchPostActions.failure({ error }),
+  ]);
 }
 
 export function* watchCreatePost() {
