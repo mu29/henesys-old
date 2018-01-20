bab063c036f404f59c4b35e103b0b74dc2e17fc1
InJung Chung <mu29@yeoubi.net>
2018-01-21 01:58:29 +0900

    store 코드 스타일 오류를 수정한다

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   distDir: '../functions/next',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    });
-    return config;
-  },
 };
--- src/app/store.js
+++ src/app/store.js
@@ -1,31 +1,36 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
-import withRedux from 'next-redux-wrapper'
-import nextReduxSaga from 'next-redux-saga'
+import withRedux from 'next-redux-wrapper';
+import nextReduxSaga from 'next-redux-saga';
 import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './sagas';
 
 let createStoreWithMiddleware;
-const loggerMiddleware = createLogger();
-const sagaMiddleware = createSagaMiddleware();
+const logger = createLogger();
+const saga = createSagaMiddleware();
 
 if (process.env.NODE_ENV === 'development') {
-  createStoreWithMiddleware = compose(
-    applyMiddleware(sagaMiddleware, loggerMiddleware),
-  )(createStore);
+  createStoreWithMiddleware = compose(applyMiddleware(saga, logger))(createStore);
 } else {
-  createStoreWithMiddleware = compose(
-    applyMiddleware(sagaMiddleware),
-  )(createStore);
+  createStoreWithMiddleware = compose(applyMiddleware(saga))(createStore);
 }
 
 export default function configureStore(initialState) {
+  /* eslint-disable */
   const reducers = require('./modules');
-  const store = createStoreWithMiddleware(combineReducers(reducers), initialState);
-  store.sagaTask = sagaMiddleware.run(rootSaga);
+  /* eslint-enable */
+  const store = createStoreWithMiddleware(
+    combineReducers(reducers),
+    initialState,
+  );
+  store.sagaTask = saga.run(rootSaga);
   return store;
 }
 
 export function withReduxSaga(BaseComponent, mapStateToProps = null, mapDispatchToProps = null) {
-  return withRedux(configureStore, mapStateToProps, mapDispatchToProps)(nextReduxSaga(BaseComponent))
+  return withRedux(
+    configureStore,
+    mapStateToProps,
+    mapDispatchToProps,
+  )(nextReduxSaga(BaseComponent));
 }
