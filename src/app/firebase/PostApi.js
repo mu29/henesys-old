eabf46100113aadb5a9e45ce7f4607d94a7c6cc7
InJung Chung <mu29@yeoubi.net>
2018-01-25 00:25:31 +0900

    PostApi 에서 오류가 난 경우 메시지를 리턴한다

--- src/app/firebase/PostApi.js
+++ src/app/firebase/PostApi.js
@@ -4,10 +4,10 @@ export async function createPost(tag, title, content) {
   const db = loadDB();
   const auth = loadAuth();
   const firebase = loadFirebase();
-  const tagRef = db.collection('tags').doc(tag);
-  const userRef = db.collection('users').doc(auth.currentUser.uid);
 
   try {
+    const tagRef = db.collection('tags').doc(tag);
+    const userRef = db.collection('users').doc(auth.currentUser.uid);
     const postRef = await db.collection('posts').add({
       title,
       content,
@@ -31,21 +31,21 @@ export async function createPost(tag, title, content) {
 export async function readPostList(tag, last) {
   const db = loadDB();
 
-  let query = db.collection('posts');
-
-  if (tag) {
-    const tagRef = db.collection('tags').doc(tag);
-    query = query.where('tag', '==', tagRef);
-  }
-
-  query = query.orderBy('createdAt', 'desc');
-
-  if (last) {
-    const lastPostRef = await db.collection('posts').doc(last).get();
-    query = query.startAfter(lastPostRef);
-  }
-
   try {
+    let query = db.collection('posts');
+
+    if (tag) {
+      const tagRef = db.collection('tags').doc(tag);
+      query = query.where('tag', '==', tagRef);
+    }
+
+    query = query.orderBy('createdAt', 'desc');
+
+    if (last) {
+      const lastPostRef = await db.collection('posts').doc(last).get();
+      query = query.startAfter(lastPostRef);
+    }
+
     const posts = await query.limit(20).get();
     return { posts: posts.docs.map(d => ({ id: d.id, ...d.data(), tag })) };
   } catch (error) {
